5092395a9df30eb88762d4b820292c1e38530d90
madi007 <60744461+madi007@users.noreply.github.com>
2024-01-25 01:39:01 +0600

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+function showSchedule() {
+    var scheduleDiv = document.getElementById('courseSchedule');
+    if (scheduleDiv.style.display === 'none') {
+        scheduleDiv.style.display = 'block';
+    } else {
+        scheduleDiv.style.display = 'none';
+    }
+}
+ 
