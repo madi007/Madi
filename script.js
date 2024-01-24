3d8cd53cb8741a132cc93b25a346dc2e626d6dbb
madi007 <60744461+madi007@users.noreply.github.com>
2024-01-25 01:07:16 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
+let display = document.getElementById('display');
+let currentExpression = '';
+
+function appendToDisplay(value) {
+    display.value += value;
+    currentExpression += value;
+}
+
+function clearDisplay() {
+    display.value = '';
+    currentExpression = '';
+}
+
 function calculate() {
     try {
-        let result = eval(currentExpression);
+        let result = Function('"use strict"; return (' + currentExpression + ')')();
         display.value = result;
         addToHistory(result);
         currentExpression = '';  // Сбросить текущее выражение после вычисления
@@ -8,4 +21,15 @@ function calculate() {
         display.value = 'Error';
         currentExpression = '';  // Сбросить текущее выражение при ошибке
     }
+}
+
+function saveResult() {
+    addToHistory(display.value);
+}
+
+function addToHistory(result) {
+    let historyList = document.getElementById('history-list');
+    let listItem = document.createElement('li');
+    listItem.textContent = result;
+    historyList.appendChild(listItem);
 }
