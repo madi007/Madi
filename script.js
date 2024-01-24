17ef90502c1c4b86a73052a6858ee2da29e2dc94
madi007 <60744461+madi007@users.noreply.github.com>
2024-01-25 00:48:38 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 let display = document.getElementById('display');
+let historyList = document.getElementById('history-list');
 
 function appendToDisplay(value) {
     display.value += value;
@@ -10,8 +11,20 @@ function clearDisplay() {
 
 function calculate() {
     try {
-        display.value = eval(display.value);
+        let result = eval(display.value);
+        display.value = result;
+        addToHistory(result);
     } catch (error) {
         display.value = 'Error';
     }
+}
+
+function saveResult() {
+    addToHistory(display.value);
+}
+
+function addToHistory(result) {
+    let listItem = document.createElement('li');
+    listItem.textContent = result;
+    historyList.appendChild(listItem);
 }
