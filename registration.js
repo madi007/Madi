10f085065ff4b55a62438ee7b55caa2b7e005277
madi007 <60744461+madi007@users.noreply.github.com>
2024-01-25 17:57:51 +0600

    Update registration.js

--- registration.js
+++ registration.js
@@ -10,17 +10,59 @@ function closeRegistrationModal() {
     modal.style.display = 'none';
 }
 
+function openLoginModal() {
+    var modal = document.getElementById('loginModal');
+    modal.style.display = 'block';
+}
+
+function closeLoginModal() {
+    var modal = document.getElementById('loginModal');
+    modal.style.display = 'none';
+}
+
 function registerUser() {
     var regUsername = document.getElementById('regUsername').value;
     var regPassword = document.getElementById('regPassword').value;
 
     // В реальном приложении отправляйте данные на сервер для обработки и сохранения в базе данных.
-    // Здесь просто выведем информацию в консоль.
+    // Здесь просто сохраним данные в localStorage.
+
+    localStorage.setItem('username', regUsername);
+    localStorage.setItem('password', regPassword);
 
     console.log('Пользователь зарегистрирован:');
     console.log('Имя пользователя:', regUsername);
     console.log('Пароль:', regPassword);
 
-    // Закрываем модальное окно после регистрации
     closeRegistrationModal();
 }
+
+function loginUser() {
+    var loginUsername = document.getElementById('loginUsername').value;
+    var loginPassword = document.getElementById('loginPassword').value;
+
+    // В реальном приложении отправляйте данные на сервер для проверки и аутентификации.
+    // Здесь просто сравним данные с теми, что сохранены в localStorage.
+
+    var storedUsername = localStorage.getItem('username');
+    var storedPassword = localStorage.getItem('password');
+
+    if (loginUsername === storedUsername && loginPassword === storedPassword) {
+        console.log('Вход выполнен успешно!');
+        closeLoginModal();
+        openUserProfile();
+    } else {
+        console.log('Ошибка входа. Проверьте имя пользователя и пароль.');
+    }
+}
+
+function openUserProfile() {
+    var storedUsername = localStorage.getItem('username');
+
+    if (storedUsername) {
+        alert('Добро пожаловать, ' + storedUsername + '!');
+    } else {
+        alert('Пожалуйста, войдите для доступа к личному кабинету.');
+    }
+}
+
