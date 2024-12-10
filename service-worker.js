9007c6d981f66ef03ebc84eff7504969bd673d61
madi007 <60744461+madi007@users.noreply.github.com>
2024-12-10 13:49:49 +0500

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,22 +1,11 @@
-self.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches.open('app-cache').then((cache) => {
-      return cache.addAll([
-        '/',
-        '/index.html',
-        '/style.css',
-        '/script.js',
-        '/img/icon-192x192.png',
-        '/img/icon-512x512.png',
-      ]);
-    })
-  );
-});
-
-self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
-    })
-  );
-});
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/service-worker.js')
+      .then((registration) => {
+        console.log('Service Worker registered with scope:', registration.scope);
+      })
+      .catch((error) => {
+        console.log('Service Worker registration failed:', error);
+      });
+  });
+}
