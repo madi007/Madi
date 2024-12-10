3c2c70a35adbd4b197140dc801f069d13a638d89
madi007 <60744461+madi007@users.noreply.github.com>
2024-12-10 14:46:16 +0500

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -4,8 +4,8 @@ const urlsToCache = [
   "/styles.css",                // Стили
   "/script.js",                 // Скрипт
   "/manifest.json",             // Манифест
-  "/img/icon-192x192.png",      // Иконка 192x192
-  "/img/icon-512x512.png",      // Иконка 512x512
+  "/img/your-icon.png",      // Иконка 192x192
+  "/img/your-icon-kaspi.png",      // Иконка 512x512
   "/img/qr.png",                // Иконка QR
   "/img/upload.png",            // Иконка загрузки
   "/img/qr-code.png",          // Иконка QR-кода
