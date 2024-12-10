9719b19460c64e5debe8f5a9fef9ea8aeb48a8ef
madi007 <60744461+madi007@users.noreply.github.com>
2024-12-10 14:33:40 +0500

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,11 +1,30 @@
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/service-worker.js')
-      .then((registration) => {
-        console.log('Service Worker registered with scope:', registration.scope);
-      })
-      .catch((error) => {
-        console.log('Service Worker registration failed:', error);
-      });
-  });
-}
+const CACHE_NAME = "id-app-cache-v1";
+const urlsToCache = [
+  "./",
+  "./index.html",
+  "./styles.css",
+  "./script.js",
+  "./manifest.json",
+  "./img/icon-192x192.png",
+  "./img/icon-512x512.png",
+  "./img/qr.png",
+  "./img/upload.png",
+  "./img/qr-code.png",
+  "./img/Back.png"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
